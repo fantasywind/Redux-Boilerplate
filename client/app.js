99a8e685f7bbc4802a2cc2699a9637e25bd6dece
William Wang <shadow25251@gmail.com>
2016-09-24 02:23:40 +0800

    add independent store

--- client/app.js
+++ client/app.js
@@ -1,41 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
-import config from './config';
-import {
-  createStore,
-  applyMiddleware,
-} from 'redux';
-import {
-  Provider,
-} from 'react-redux';
+import { Provider } from 'react-redux';
+import store from './store/configureStore';
 import {
   Router,
   Route,
   browserHistory,
   IndexRoute,
 } from 'react-router';
-import {
-  syncHistoryWithStore,
-  routerMiddleware,
-} from 'react-router-redux';
-import apiMiddleware from './middlewares/apiMiddleware.js';
-import reducers from './reducers';
+import { syncHistoryWithStore } from 'react-router-redux';
 
-const logger = createLogger({
-  collapsed: (getState, action) => (action.type.includes('@@router/')),
-});
-
-const middlewares = [thunk, routerMiddleware(browserHistory), apiMiddleware];
-
-if (config.useReduxLog) {
-  middlewares.push(logger);
-}
-
-export const store = createStore(reducers, {}, applyMiddleware(...middlewares));
-
-const history = syncHistoryWithStore(browserHistory, store);
 
 // Boards
 import App from './components/App.js';
@@ -43,7 +17,7 @@ import LandingPage from './components/LandingPage.js';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
+    <Router history={syncHistoryWithStore(browserHistory, store)}>
       <Route path="/" component={App}>
         <IndexRoute component={LandingPage} />
       </Route>
