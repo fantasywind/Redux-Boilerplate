e9113414f688ba7ed56a08d6978b0b67b93dec37
William Wang <shadow25251@gmail.com>
2016-09-24 12:16:39 +0800

    i18n confing done

--- client/app.js
+++ client/app.js
@@ -8,22 +8,33 @@ import {
 } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
+// i18n
+import { I18nextProvider } from 'react-i18next';
+import i18n from './libs/i18n/';
 // Boards
 import AppPage from './components/App.js';
 import LandingPage from './components/LandingPage.js';
 
+
 class App extends Component {
   render() {
-    const {
-      store
-    } = this.props;
+    const { store } = this.props;
+
+    const Routes =
+    (<Router history={syncHistoryWithStore(browserHistory, store)}>
+      <Route path="/" component={AppPage}>
+        <IndexRoute component={LandingPage} />
+      </Route>
+    </Router>);
+
+
     return (
       <Provider store={store}>
-        <Router history={syncHistoryWithStore(browserHistory, store)}>
-          <Route path="/" component={AppPage}>
-            <IndexRoute component={LandingPage} />
-          </Route>
-        </Router>
+        <I18nextProvider i18n={i18n}>
+          <div>
+            {Routes}
+          </div>
+        </I18nextProvider>
       </Provider>
     );
   }
--- client/config.js
+++ client/config.js
@@ -1,3 +1,9 @@
 export default {
   useReduxLog: true,
+  i18n: {
+    remoteLanguageResource: false,
+    browserLanguagedetector: true,
+    localStorageCache: false,
+    debug: false,
+  }
 };
